a627e34f8bfebdcd13987d5eb78699ddfcd097d7
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 18:21:39 +0100

    Initial Commit

---
+++ Text/Text.h
@@ -0,0 +1,19 @@
+//
+//  Text.h
+//  Text
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Text.
+FOUNDATION_EXPORT double TextVersionNumber;
+
+//! Project version string for Text.
+FOUNDATION_EXPORT const unsigned char TextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Text/PublicHeader.h>
+
+
